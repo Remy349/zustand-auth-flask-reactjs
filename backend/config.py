adb5f022355bb509796f9fcb832ca7c7a6830afc
Remy349 <santoraga15@gmail.com>
2023-09-30 15:48:30 -0600

    Added initial files and first commit.
    Basic setup workspace.

---
+++ backend/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config(object):
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
+    API_TITLE = "Auth REST API - Zustand | ReactJS"
+    API_VERSION = "v1"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/docs"
+
+
+class DevelopmentConfig(Config):
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.db")
